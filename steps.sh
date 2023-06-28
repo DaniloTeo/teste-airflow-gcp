3e0ede610a032d30808283844fa4f48c626c4ca1
Danilo T <daniloctteo@gmail.com>
2023-06-28 10:18:54 -0300

    chore: partial script for docker installation

---
+++ steps.sh
@@ -0,0 +1,16 @@
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install git -y
+for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")3" stable" |   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+git clone https://github.com/DaniloTeo/teste-airflow-gcp.git
+cd teste-airflow-gcp/
+sudo docker compose up -d
